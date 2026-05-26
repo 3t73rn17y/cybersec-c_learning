46aeeb1b8d591509899016aa22e0adddea211688
3t73rn17y <254739606+3t73rn17y@users.noreply.github.com>
2026-05-26 23:07:03 +0200

    did something I guess

--- 1_Caesar_Cipher_CLI_Tool/main.c
+++ 1_Caesar_Cipher_CLI_Tool/main.c
@@ -9,7 +9,16 @@ int main (int c, char **v){
 
 	//decleration and assignmnet of file read function etc:
 	FILE *file = fopen(v[1], "r");
-	printf("file has been read successfully");
+	//printf("file has been read successfully");
+	
+	//declare an int for the characters in the file
+	//do the shit to make ceasars cipher possible!
+	int ch;
+    while ((ch = fgetc(file)) != EOF)
+    {
+        printf("%c", ch);
+    }
+
 
 	fclose(file);
 	return 0;
